e71769dfbe27ba61fea37cf747502c159a0fe47a
YODHA0000 <lionsheart1947@gmail.com>
2024-11-05 11:24:44 +0530

    made to run

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/', // Replace 'portfolio' with your repository name
+  // Remove the base URL to fix deployment issues
 });
